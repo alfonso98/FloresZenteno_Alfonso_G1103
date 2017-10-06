6cbaf6ca5ff8fa4fc5d58b70fac4a223a0258166
alfonso98 <32371304+alfonso98@users.noreply.github.com>
2017-10-05 22:52:25 -0500

    Rename 02_palabrasPuts.c to FZA_02_palabrasPuts.c

--- FZA_02_palabrasPuts.c
+++ FZA_02_palabrasPuts.c
@@ -1,7 +1,7 @@
 // 2)
 
-// Descarga el siguiente archivo y observa su salida. Descrubre qué sucede
-// si cambias los números que se encuentran entre los paréntesis.
+// Descarga el siguiente archivo y observa su salida. Descrubre quÃ© sucede
+// si cambias los nÃºmeros que se encuentran entre los parÃ©ntesis.
 
 #include <stdio.h>
 
@@ -16,7 +16,7 @@ puts(palabras);
 puts(&nombre[8]);
 
 //El programa guarda en un arreglo una serie de caracteres y le asigna a cada 
-//caracter su propio espacio en memoria, al imprimir el arreglo cambiando el número en los corchetes
+//caracter su propio espacio en memoria, al imprimir el arreglo cambiando el nÃºmero en los corchetes
 //se comienza a imprimir desde el caracter gusrdado en el espacio de memoria especificado.
 
 
